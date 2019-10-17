d5080b63d8f2f180ca305df5ee6bcf6a954bd247
Pikaoku <pikaoku@gmail.com>
2019-10-17 21:25:36 +0100

    Chore: add playground functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,51 @@
-export const WeeTest = (name: string) => `Test ${name}`
+export type Request = (...args: any) => any
 
-export type Cooker<Model> = (data?: Partial<Model>) => Model
+export type PromiseRequest = (...args: any) => Promise<any>
 
-export function cook<Model>(data: Partial<Model> = {}, template: Model): Model {
-	return { ...template, ...data }
+import { Subscribable } from 'rxjs'
+
+interface HasOptions {
+	options?: object
+}
+interface HasData {
+	data: object
+}
+interface HasId {
+	id: string
+}
+
+export interface RequestNode<RequestType> {
+	[key: string]: RequestType | RequestNode<RequestType>
+}
+
+export type PromiseRequestNode = RequestNode<PromiseRequest>
+
+export interface SimpleCrud<Model> {
+	create: ({  }: HasData & HasOptions) => Promise<string>
+	read: ({  }: HasId & HasOptions) => Promise<Model>
+	update: ({  }: HasId & HasData & HasOptions) => Promise<void>
+	delete: ({  }: HasId & HasOptions) => Promise<void>
+}
+
+export interface HasStreams<Model> {
+	lists: {
+		[key: string]: Subscribable<Model>
+	}
+}
+
+export interface PikAPI {
+	[key: string]: PromiseRequestNode
+}
+
+interface User {
+	id: 'hello'
+	age: number
+	status: 'active' | 'disabled'
+}
+
+const api: SimpleCrud<User> = {
+	create: ({ data }) => Promise.resolve().then(() => JSON.stringify(data)),
+	delete: ({ id }) => Promise.resolve(),
+	read: ({ id }) => Promise.resolve().then(() => ({} as User)),
+	update: ({ id, data }) => Promise.resolve(),
 }
