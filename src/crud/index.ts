7bfb0d236186b2fcc8edf6c7af1c68b94f7f2772
Pikaoku <pikaoku@gmail.com>
2019-10-20 05:43:23 +0100

    Refactor: tidy the code

--- src/crud/index.ts
+++ src/crud/index.ts
@@ -81,7 +81,7 @@ export function createCrud<Model>({
 						id,
 						options,
 					})
-					.then(withMiddleware(r))
+					.then(withMiddleware(u))
 					.then(),
 			delete: ({ id, options = {} }) =>
 				d
